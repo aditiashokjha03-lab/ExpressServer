ae94f370fb7cd8387b91207eb4ed15d941101722
Aditi Jha <aditiashokjha03@gmail.com>
2026-01-14 18:59:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
+// Root route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the root route!' });
+});
+
+// Other routes
+app.get('/home', (req, res) => {
+  res.json({ message: 'This is home page' });
+});
+
+app.get('/contactus', (req, res) => {
+  res.json({ message: 'Contact us at contact@contact.com' });
+});
+
+app.get('/about', (req, res) => {
+  res.json({ message: 'Welcome to the About page!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
